79d5204a4a3c03f6fb800135f1f2345a64f88b5f
rgv176 <ragavimahenthiran@gmail.com>
2022-10-17 11:03:45 +0200

    feat: remove unnecessary code

--- lab3/src/index.js
+++ lab3/src/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const bodyParser = require("body-parser");
 const routerArticles = require("./routes/articles");
 const routerComments = require("./routes/comments");
-app.use(bodyParser.json());
+
 app.use("/articles", routerArticles);
 app.use("/comments", routerComments);
 
