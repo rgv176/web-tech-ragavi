1fc2c4d08d36ce2140ab2a144f70815328a87712
rgv176 <ragavimahenthiran@gmail.com>
2022-10-17 15:53:18 +0200

    feat: create differente page router

---
+++ lab3/app/pages/articles/index.js
@@ -0,0 +1,11 @@
+function ArticleList() {
+  return (
+    <>
+      <h2>Article 1</h2>
+      <h2>Article 2</h2>
+      <h2>Article 3</h2>
+      <h2>Article 4</h2>
+    </>
+  );
+}
+export default ArticleList;
---
+++ lab3/app/pages/contacts.js
@@ -0,0 +1,5 @@
+function Contact() {
+  return <h1>Contacts</h1>;
+}
+
+export default Contact;
