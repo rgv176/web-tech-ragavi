0fc154535aa5bc60b0ec8f144f15a88c3f797c1c
rgv176 <ragavimahenthiran@gmail.com>
2022-11-07 11:04:44 +0100

    feat: add content to pages

--- lab4/app/pages/about.js
+++ lab4/app/pages/about.js
@@ -3,9 +3,21 @@ function About() {
   return (
     <>
       <h2>About</h2>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
+
+      <p>
+        Lorem ips dolor sit amet, consectetur adipisci elit, sed eiusmod tempor
+        incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+        nostr exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid
+        ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate
+        velit esse cill dolore eu fugiat nulla pariatur. Excepteur sint obcaecat
+        cupiditat non proident, sunt in culpa qui officia deserunt mollit anim
+        id est labor.
+      </p>
+      <h3>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </h3>
       <footer>
         <p>Author: Ragavi and Claude</p>
       </footer>
--- lab4/app/pages/contacts.js
+++ lab4/app/pages/contacts.js
@@ -4,9 +4,21 @@ function Contact() {
   return (
     <>
       <h2>Contacts</h2>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
+
+      <p>
+        Lorem pisum dolor sit amet, consectetur adpisci elit, sed eiusmod tempor
+        incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+        nostrum exercitationem ullam corporis suscpit laboriosam, nisi ut
+        aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+        sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <h3>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </h3>
       <footer>
         <p>Author: Ragavi and Claude</p>
       </footer>
