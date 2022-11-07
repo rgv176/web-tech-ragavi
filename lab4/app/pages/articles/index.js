e1f9037ccb5ed57931e8532917bb3b1dc378513f
rgv176 <ragavimahenthiran@gmail.com>
2022-11-07 11:00:50 +0100

    feat: add content to articles

--- lab4/app/pages/articles/index.js
+++ lab4/app/pages/articles/index.js
@@ -3,29 +3,54 @@ import Link from "next/link";
 function ArticleList() {
   return (
     <>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
+      <h1>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </h1>
       <h2>
         <Link href="/articles/1">
           <a>Article 1</a>
         </Link>
       </h2>
+
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt,
+        explicabo.
+      </p>
       <h2>
         <Link href="/articles/2">
           <a>Article 2</a>
         </Link>
       </h2>
+      <p>
+        Temporibus autem quibusdam et aut officiis debitis aut rerum
+        necessitatibus saepe eveniet, ut et voluptates repudiandae sint et
+        molestiae non recusandae.
+      </p>
+
       <h2>
         <Link href="/articles/3">
-          <a>Article 3</a>
+          <a>Article3</a>
         </Link>
       </h2>
+      <p>
+        Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+        voluptatibus maiores alias consequatur aut perferendis doloribus
+        asperiores repellat.
+      </p>
       <h2>
         <Link href="/articles/4">
           <a>Article 4</a>
         </Link>
       </h2>
+      <p>
+        Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+        voluptatibus maiores alias consequatur aut perferendis doloribus
+        asperiores repellat.
+      </p>
       <footer>
         <p>Author: Ragavi and Claude</p>
       </footer>
