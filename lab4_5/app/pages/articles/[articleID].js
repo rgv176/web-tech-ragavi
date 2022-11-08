40339e6a5d0e979c6726a8874fc14e20e80a6986
rgv176 <ragavimahenthiran@gmail.com>
2022-11-08 22:31:57 +0100

    fix: dynamic code for articles

--- lab4_5/app/pages/articles/[articleID].js
+++ lab4_5/app/pages/articles/[articleID].js
@@ -1,22 +1,17 @@
 import { useRouter } from "next/router";
 import Layout from "../../component/Layout.js";
 
-function ArticleDetail() {
+function ArticleDetail({ article }) {
   const Router = useRouter();
   const articleID = Router.query.articleID;
   return (
     <>
       <Layout>
         <h1 class="w-full px-20 py-4 text-2xl underline underline-offset-8 ">
-          Details of Article {articleID}
+          {article.title}
         </h1>
         <br></br>
-        <p>
-          Lorem ips dolor sit amet, consectetur adipisci elit, sed eiusmod
-          tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostr exercitationem ullam corporis suscipit laboriosam,
-          nisi ut aliquid ex ea commodi consequatur.
-        </p>
+        <p>{article.message}</p>
       </Layout>
     </>
   );
