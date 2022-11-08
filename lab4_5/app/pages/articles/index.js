6c9d30cf3188532bbf098e2673b34ccce986a107
rgv176 <ragavimahenthiran@gmail.com>
2022-11-08 22:25:35 +0100

    fix: add dynamic code for articles

--- lab4_5/app/pages/articles/index.js
+++ lab4_5/app/pages/articles/index.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Layout from "../../component/Layout.js";
 
-function ArticleList() {
+function ArticleList({ articles }) {
   return (
     <>
       <Layout>
@@ -9,51 +9,16 @@ function ArticleList() {
           Articles
         </h1>
         <br></br>
-        <h2>
-          <Link href="/articles/1">
-            <a>Article 1</a>
-          </Link>
-        </h2>
-
-        <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt, explicabo.
-        </p>
-        <br></br>
-        <h2>
-          <Link href="/articles/2">
-            <a>Article 2</a>
-          </Link>
-        </h2>
-        <p>
-          Temporibus autem quibusdam et aut officiis debitis aut rerum
-          necessitatibus saepe eveniet, ut et voluptates repudiandae sint et
-          molestiae non recusandae.
-        </p>
-        <br></br>
-        <h2>
-          <Link href="/articles/3">
-            <a>Article3</a>
-          </Link>
-        </h2>
-        <p>
-          Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
-          voluptatibus maiores alias consequatur aut perferendis doloribus
-          asperiores repellat.
-        </p>
-        <br></br>
-        <h2>
-          <Link href="/articles/4">
-            <a>Article 4</a>
-          </Link>
-        </h2>
-        <p>
-          Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
-          voluptatibus maiores alias consequatur aut perferendis doloribus
-          asperiores repellat.
-        </p>
+        <ul>
+          {articles.map((article) => (
+            <li key={article.slug} className="my-5">
+              <h2 className="font-bold mb-1">
+                <Link href={`/articles/${article.slug}`}>{article.title}</Link>
+              </h2>
+              <p>{article.message}</p>
+            </li>
+          ))}
+        </ul>
       </Layout>
     </>
   );
