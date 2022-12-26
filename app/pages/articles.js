e3abcd370fbd796f861b9ecbe517efdb6fc71a08
rgv176 <ragavimahenthiran@gmail.com>
2022-12-26 22:23:58 +0100

    fix: personnal link authentification

--- app/pages/articles.js
+++ app/pages/articles.js
@@ -1,11 +1,9 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Layout from '../components/Layout.js'
-import { supabase } from './api/supabase'
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../components/Layout.js";
+import { supabase } from "./api/supabase";
 
-export default function Articles({
-  articles
-}) {
+export default function Articles({ articles }) {
   return (
     <Layout>
       <Head>
@@ -13,31 +11,30 @@ export default function Articles({
         <meta name="description" content="WebTech articles page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className='wt-title'>
-        Web technologies articles
-      </h1>
-      <p className="italic my-5">This page fetches data at build time, excellent for SEO.</p>
+      <h1 className="wt-title">Web technologies articles</h1>
       <ul>
-        {articles.map( article => (
+        {articles.map((article) => (
           <li key={article.slug} className="my-5">
-            <h2 className="font-bold mb-1"><Link href={`/articles/${article.slug}`}>{article.title}</Link></h2>
+            <h2 className="font-bold mb-1">
+              <Link href={`/articles/${article.slug}`}>{article.title}</Link>
+            </h2>
             <p>{article.message}</p>
           </li>
         ))}
       </ul>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps(ctx) {
-  let articles = []
+  let articles = [];
   let { data, error, status } = await supabase
-    .from('articles')
-    .select(`id, slug, message, title`)
-  if (!error) articles = data // handle errors
+    .from("articles")
+    .select(`id, slug, message, title`);
+  if (!error) articles = data; // handle errors
   return {
     props: {
-      articles: articles
-    }
+      articles: articles,
+    },
   };
 }
--- app/pages/articles/[slug].js
+++ app/pages/articles/[slug].js
@@ -1,12 +1,10 @@
-import { useEffect, useState } from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
-import Layout from '../../components/Layout.js'
-import { supabase } from '../api/supabase'
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/Layout.js";
+import { supabase } from "../api/supabase";
 
-export default function Article({
-  article
-}) {
+export default function Article({ article }) {
   return (
     <Layout>
       <Head>
@@ -14,40 +12,33 @@ export default function Article({
         <meta name="description" content="WebTech articles page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className='wt-title'>
-        {article.title}
-      </h1>
-      <p className="italic my-5">This page fetches data at build time, excellent for SEO.</p>
-      <p>
-        {article.message}
-      </p>
+      <h1 className="wt-title">{article.title}</h1>
+      <p>{article.message}</p>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps(ctx) {
-  let article = {}
+  let article = {};
   let { data, error, status } = await supabase
-    .from('articles')
+    .from("articles")
     .select(`id, slug, message, title`)
-    .eq('slug', ctx.params.slug)
-    .single()
-  if (!error) article = data // handle errors
+    .eq("slug", ctx.params.slug)
+    .single();
+  if (!error) article = data; // handle errors
   return {
     props: {
-      article: article
-    }
+      article: article,
+    },
   };
 }
 
 export async function getStaticPaths(ctx) {
-  let articles = []
-  let { data, error, status } = await supabase
-    .from('articles')
-    .select(`slug`)
-  if (!error) articles = data // handle errors
+  let articles = [];
+  let { data, error, status } = await supabase.from("articles").select(`slug`);
+  if (!error) articles = data; // handle errors
   return {
-    paths: articles.map( article => `/articles/${article.slug}`),
-    fallback: false
+    paths: articles.map((article) => `/articles/${article.slug}`),
+    fallback: false,
   };
 }
