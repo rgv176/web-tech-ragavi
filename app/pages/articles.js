f0fd0f2c3cb86e25023c0609662591e520d3246d
rgv176 <ragavimahenthiran@gmail.com>
2022-12-27 17:22:16 +0100

    fix: fetch datas

--- app/pages/articles.js
+++ app/pages/articles.js
@@ -1,9 +1,21 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 import Layout from "../components/Layout.js";
 import { supabase } from "./api/supabase";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
-export default function Articles({ articles }) {
+export default function Articles() {
+  const [datas, setDatas] = useState([]);
+  const supabase = useSupabaseClient();
+  useEffect(() => {
+    (async () => {
+      let { data, error, status } = await supabase
+        .from("articles")
+        .select(`id, title, slug, message`);
+      setDatas(data);
+    })();
+  }, [supabase]);
   return (
     <Layout>
       <Head>
@@ -13,7 +25,7 @@ export default function Articles({ articles }) {
       </Head>
       <h1 className="wt-title">Web technologies articles</h1>
       <ul>
-        {articles.map((article) => (
+        {datas.map((article) => (
           <li key={article.slug} className="my-5">
             <h2 className="font-bold mb-1">
               <Link href={`/articles/${article.slug}`}>{article.title}</Link>
